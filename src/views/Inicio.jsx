79b6af2318814f8a18fa8613167cf6944a1ac4e5
Miguel Ángel García <magarcial.dev@outlook.es>
2025-09-22 18:04:57 +0200

    Use useSWR to revalidate info and get products

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,25 +1,35 @@
+import useSWR from 'swr';
 import Producto from '../components/Producto';
-import { productos as productosData } from '../data/productos';
 import { useRestaurante } from '../hooks/useRestaurante';
+import { clienteAxios } from '../helpers';
 
 export default function Inicio() {
-	const { categorias, categoriaActual } = useRestaurante();
+  const { categorias, categoriaActual } = useRestaurante();
 
-	const productos = productosData.filter(
-		(producto) => producto.categoria_id === categoriaActual.id
-	);
+  // Consulta SWR
+  const fetcher = () =>
+    clienteAxios('/api/productos').then((data) => data.data);
+  const { data, error, isLoading } = useSWR('api/productos', fetcher, {
+    refreshInterval: 1000,
+  });
 
-	return (
-		<>
-			<h1 className="text-4xl font-black">{categoriaActual.nombre}</h1>
-			<p>Elige y personaliza tu pedido a continuación</p>
+  if (isLoading) return 'Cargnado...';
 
-			<div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-10">
-				{productos.map((producto) => (
-					<Producto key={producto.id} producto={producto} />
-				))}
-			</div>
-		</>
-	);
+  const productos = data.data.filter(
+    (producto) => producto.categoria_id === categoriaActual.id
+  );
+
+  return (
+    <>
+      <h1 className="text-4xl font-black">{categoriaActual.nombre}</h1>
+      <p>Elige y personaliza tu pedido a continuación</p>
+
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mt-10">
+        {productos.map((producto) => (
+          <Producto key={producto.id} producto={producto} />
+        ))}
+      </div>
+    </>
+  );
 }
 
